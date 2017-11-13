bfd86d5d0909ca2ccaf403923084589a53f6aaf0
FRANCISCO JOSE SANCHEZ BAO <francisco.sanchez@tismart.com>
2017-11-13 07:41:34 -0500

    Initial Commit

---
+++ BaseProyectFinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaseProyectFinal
+//
+//  Created by FRANCISCO JOSE SANCHEZ BAO on 13/11/17.
+//  Copyright © 2017 CIBERTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
